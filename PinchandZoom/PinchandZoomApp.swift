cfe16b2adf319773c6acbc656955a66d926e8877
Selin Çağlar <58820720+selincsc@users.noreply.github.com>
2022-11-15 18:48:23 +0300

    Initial Commit

---
+++ PinchandZoom/PinchandZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchandZoomApp.swift
+//  PinchandZoom
+//
+//  Created by Selin Çağlar on 15.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchandZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
